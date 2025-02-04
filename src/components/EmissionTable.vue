3255fd5bcec239b58bcd2fc6a8832797901d2453
Julia Wuerthele <julia.wuerthele@gmx.de>
2025-02-04 22:21:40 +0100

    adds broken sorting function

--- src/components/EmissionTable.vue
+++ src/components/EmissionTable.vue
@@ -7,20 +7,51 @@ const data = ref(emissionData);
 const countries = Array.from(new Set(emissionData.map(a => a.country))).sort();
 const currentlySelectedCountries = ref([]);
 const currentlySelectedCompany = ref('');
+const sorting = ref('lth-company');
 
-function checkCountry(company) {    
+function checkCountry(company) {
     return currentlySelectedCountries.value.includes(company.country);
 }
 
-function filter() {
+function sortByName(a, b) {
+    const nameA = a.toUpperCase(); // ignore upper and lowercase
+    const nameB = b.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+        return -1;
+    }
+    if (nameA > nameB) {
+        return 1;
+    }
+    return 0;
+}
+
+function filterAndSort() {
     if (currentlySelectedCountries.value.length > 0) {
         data.value = emissionData.filter(checkCountry);
     } else {
         data.value = emissionData;
     }
     if (currentlySelectedCompany.value !== '') {
-        console.log(data.value.filter((company) => company.name === currentlySelectedCompany.value));
         data.value = data.value.filter((company) => company.name === currentlySelectedCompany.value);
+    }
+    switch (sorting) {
+        case 'htl-company':
+            data.value.sort((a, b) => sortByName(b.company, a.company));
+            break;
+        case 'lth-country':
+            data.value.sort((a, b) => sortByName(a.country, b.country));
+            break;
+        case 'htl-country':
+            data.value.sort((a, b) => sortByName(a.country, b.country));
+            break;
+        case 'lth-emissions':
+            data.value.sort((a, b) => parseFloat(a.emissions) - parseFloat(b.emissions));
+            break;
+        case 'htl-emissions':
+            data.value.sort((a, b) => parseFloat(b.emissions) - parseFloat(a.emissions));
+            break;
+        default:
+            data.value.sort((a, b) => sortByName(a.company, b.company));
     }
 }
 </script>
@@ -37,10 +68,21 @@ function filter() {
                             :index="country" />
                         <label :for="country">{{ country }}</label>
                     </li>
-                </ul>                
+                </ul>
             </fieldset>
             <CompanyFilter v-model="currentlySelectedCompany"></CompanyFilter>
-            <button @click.prevent="filter">Anwenden</button>
+            <label>
+                Sortierung:
+                <select v-model="sort">
+                    <option value="lth-company">alphabetisch aufsteigend nach Unternehmen</option>
+                    <option value="htl-company">alphabetisch absteigend nach Unternehmen</option>
+                    <option value="lth-country">alphabetisch aufsteigend nach Land</option>
+                    <option value="htl-country">alphabetisch absteigend nach Land</option>
+                    <option value="lth-emissions">aufsteigend nach CO<sub>2</sub>-Emissionen</option>
+                    <option value="htl-emissions">absteigend nach CO<sub>2</sub>-Emissionen</option>
+                </select>
+            </label>
+            <button @click.prevent="filterAndSort">Anwenden</button>
         </form>
         <table class="table" id="emissions-table">
             <thead>
@@ -54,7 +96,7 @@ function filter() {
                 <tr v-for="{ name, country, emissions } in data" :key="name">
                     <td>{{ name }}</td>
                     <td>{{ country }}</td>
-                    <td>{{ emissions }}</td>
+                    <td>{{ parseFloat(emissions).toFixed(2) }}</td>
                 </tr>
             </tbody>
         </table>
